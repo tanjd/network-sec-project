4de2e06f904acb32d4dee1311211cf76491c04b2
tanjd <42729752+tanjd@users.noreply.github.com>
2022-04-12 20:26:30 +0800

    clean up folder

--- demo/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-connection.connect((socket.gethostname(), 8000))
-
-while True:
-
-    message = connection.recv(1024)
